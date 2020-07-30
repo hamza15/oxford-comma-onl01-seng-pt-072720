aa73b688d8d1eab6b05da57c31fd3d245ac0cec1
Sardar Hamza Naqi <sardarhamzanaqi@gmail.com>
2020-07-30 06:31:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,10 @@
 def oxford_comma(array)
-  word = array[-1]
-  array[-1] = "and #{word}"
-  puts array.join(", ")
+  if array.size < 2 
+    puts array.join("")
+  elsif array.size == 2:
+    puts array.join(" and ")
+  else
+    word = array[-1]
+    array[-1] = "and #{word}"
+    puts array.join(", ")
 end
